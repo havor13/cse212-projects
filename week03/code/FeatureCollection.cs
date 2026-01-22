bc6637512ea45af0f3c202a9695d4cb602d56f66
havor13 <sampsonhavor43@gmail.com>
2026-01-22 07:53:47 +0000

    new updates

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,22 @@
+using System.Collections.Generic;
+
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    // Root object: contains a list of features
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature
+{
+    // Each feature has properties (place, mag, etc.)
+    public Properties Properties { get; set; }
+}
+
+public class Properties
+{
+    // Magnitude can be null, so use nullable double
+    public double? Mag { get; set; }
+
+    // Location description
+    public string Place { get; set; }
 }
